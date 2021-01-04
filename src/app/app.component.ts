ae7b06c59f3c35e5999c940084983fa1f2cd7e4e
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2021-01-03 19:20:37 -0600

    fixed display of table with <td><tr></tr></td>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+
+  sourceList: Satellite[];
+
+  constructor () {
+    this.sourceList = [
+      new Satellite("SiriusXM", "Communication", true, "LOW", "2009-03-21"),
+      new Satellite("Cat Scanner", "Imaging", true, "LOW", "2012-01-05"), 
+      new Satellite("Weber Grill", "Space Debris", false, "HIGH", "1996-03-25"), 
+      new Satellite("GPS 938", "Positioning", true, "HIGH", "2001-11-01"), 
+      new Satellite("ISS", "Space Station", true, "LOW", "1998-11-20"),
+    ];
+   }
+
+//   constructor() {
+//     this.sourceList = [];
+//     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+//     window.fetch(satellitesUrl).then(function(response) {
+//        response.json().then(function(data) {
+ 
+//           let fetchedSatellites = data.satellites;
+//           // TODO: loop over satellites
+//           // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+//           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+ 
+//        }.bind(this));
+//     }.bind(this));
+ 
+//  }
+
 }
