6d34986ba05ad20c5f5f090a04cf1127f97c9f6b
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2021-01-04 20:31:42 -0600

    finished part 8 search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   title = 'orbit-report';
 
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   // constructor () {
   //   this.sourceList = [
@@ -22,6 +23,7 @@ export class AppComponent {
   //  }
 
   constructor() {
+    this.displayList = []
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
@@ -39,8 +41,26 @@ export class AppComponent {
             // console.log(Satellite);
             this.sourceList.push(newSatellite);
           }
+          // make a copy of the sourceList to be shown to the user
+      this.displayList = this.sourceList.slice(0);
  
        }.bind(this));
     }.bind(this));
+    
  }
+ 
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
+
 }
